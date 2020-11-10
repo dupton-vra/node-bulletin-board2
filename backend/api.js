a0a77d50f4aa18bf64e60fdfaf3a27c0fd7064cb
dupton-vra <67922920+dupton-vra@users.noreply.github.com>
2020-11-10 10:19:45 -0500

    Update api.js

--- backend/api.js
+++ backend/api.js
@@ -11,7 +11,7 @@ exports.events = function (req, res) {
     //res.json(events);
     connection.query('SELECT * from events', function (err, rows, fields) {
       if (err) throw err
-          console.log('The solution is: ')
+      console.log('The solution is: ')
       var objs = [];
       for (var i = 0;i < rows.length; i++) {
           objs.push({title: rows[i].title,
@@ -28,5 +28,12 @@ exports.events = function (req, res) {
 };
 
 exports.event = function (req, res) {
-  res.json(events[req.param.eventId]);
+  //res.json(events[req.param.eventId]);
+      connection.query('SELECT * from events where id=' + req.param.eventId, function (err, rows, fields) {
+      if (err) throw err
+      console.log('The solution is: ')
+      var obj = "{title:" + rows[0].title + "," + "details:" + rows[0].details + "," + "date:" + rows[0].date + "}"
+      connection.end()
+      res.end(obj);
+    }
 };
