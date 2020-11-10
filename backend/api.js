6aed1818324fda69a545a65599bcf73dfdd53eb7
dupton-vra <67922920+dupton-vra@users.noreply.github.com>
2020-11-10 10:06:44 -0500

    Update api.js

--- backend/api.js
+++ backend/api.js
@@ -1,7 +1,30 @@
 var events = require('./events.js');
+var mysql = require('mysql')
+var connection = mysql.createConnection({
+  host: 'mysql',
+  user: 'root',
+  password: 'password',
+  database: 'DTEST1
+})
 
 exports.events = function (req, res) {
-  res.json(events);
+    //res.json(events);
+    connection.query('SELECT * from events', function (err, rows, fields) {
+      if (err) throw err
+          console.log('The solution is: ')
+      var objs = [];
+      for (var i = 0;i < rows.length; i++) {
+          objs.push({title: rows[i].title,
+                     details: rows[i].details,
+                     date: rows[i].date
+                    });
+      }
+      connection.end()
+      res.end(JSON.stringify(objs));
+    }
+}
+
+  
 };
 
 exports.event = function (req, res) {
