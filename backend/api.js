af7524d3051a6c099275e6b73bc8d05159a0fa5f
dupton-vra <67922920+dupton-vra@users.noreply.github.com>
2021-03-05 15:12:43 -0500

    Update api.js

--- backend/api.js
+++ backend/api.js
@@ -3,7 +3,7 @@
 var events = [];
 var mysql = require('sync-mysql') ;
 var client = new mysql({
-  host: '192.168.195.4',
+  host: '192.168.195.37',
   user: 'root',
   password: 'password',
   database: 'DTEST1'
