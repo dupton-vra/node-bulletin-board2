752fce43535b586cd21d5f2ec79acedc642555cf
dupton-vra <67922920+dupton-vra@users.noreply.github.com>
2021-03-16 09:55:19 -0400

    Update test.js

--- test.js
+++ test.js
@@ -1,7 +1,8 @@
 //node test2.js
 var mysql = require('sync-mysql') ;
 var client = new mysql({
-  host: '192.168.195.37',
+  host: '10.149.80.140',
+  port:31000,
   user: 'root',
   password: 'password',
   database: 'DTEST1'
