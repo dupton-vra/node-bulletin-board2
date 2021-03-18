4e93e5dd7b12984c4e904f197a250666e286384f
dupton-vra <67922920+dupton-vra@users.noreply.github.com>
2021-03-17 22:36:21 -0400

    Update api.js

--- backend/api.js
+++ backend/api.js
@@ -55,8 +55,8 @@ exports.event = function (req, res) {
        res.json(events[rows[0].id]);
      } else {
         console.log("req: " + req);
-        console.log("req1: " + req[0]);
-        console.log("req2: " + req.toString());
+        console.log("req1: " + req.getOwnPropertyNames());
+        //console.log("req2: " + req.toString());
      }
       
       //var rows = client.query("INSERT INTO events (id,title, details, date) VALUES (null,"+ req.param.title + " ,"+ req.param.details + "," + req.param.date + ");");
