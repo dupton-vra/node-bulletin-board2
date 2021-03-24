a79e186bf045a8e8fae972f5bf66b9ea5aafcd56
dupton-vra <67922920+dupton-vra@users.noreply.github.com>
2021-03-24 07:50:50 -0400

    Update app.js

--- app.js
+++ app.js
@@ -47,19 +47,10 @@ new Vue({
         {
             console.log("ER: " + e);     
         }*/
-        //this.events.push(this.event);
+        this.events.push(this.event);
         this.$http.post('/api/events', this.event)
           .success(function (res) {
-            //this.events.push(this.event);
-            this.$http.get('/api/events')
-                  .success(function (events) {
-                        this.$set('events', events);
-                        //this.event = { title: '', detail: '', date: '' };
-                        console.log(events);
-                   })
-                  .error(function (err) {
-                        console.log(err);
-                  });
+            this.events.push(this.event);
             console.log('Event added!');
           })
           .error(function (err) {
@@ -75,19 +66,11 @@ new Vue({
         console.log("ID1: " + id)
         this.$http.delete('api/events/' + id)
           .success(function (res) {
-            //var index = this.events.find(x => x.id === id)
-            //console.log("index: " + index);
-            //this.events.splice(index - 1 , 1);
-               this.$http.get('/api/events')
-                  .success(function (events) {
-                        this.$set('events', events);
-                        //this.event = { title: '', detail: '', date: '' };
-                        console.log(events);
-                   })
-                  .error(function (err) {
-                        console.log(err);
-                  });   
-            
+            console.log("RES: " + res);
+            console.log("ID: " + id)
+            var index = this.events.find(x => x.id === id)
+            console.log("index: " + index);
+            this.events.splice(index, 1);
           })
           .error(function (err) {
             console.log("ERR: " + err);
